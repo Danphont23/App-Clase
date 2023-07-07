3dc6e30a011ace4febceeded99f2ec3bac0a6a9b
¨luis <luis.gonzalez.dpht@gmail.com>
2023-07-06 23:40:04 -0400

    ok

--- src/server.js
+++ src/server.js
@@ -1,14 +1,53 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
 
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
 });
 
-const puerto = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+app.get("/app/client", (req, res) => {
+  res.render("client");
+});
+
+app.get("/app/admin", (req, res) => {
+  res.render("admin");
+});
+
+io.on("connection", (socket) => {
+  console.log("nuevo usuario conectado");
+
+  socket.on("offer", (offer) => {
+    console.log("new offer from ", socket.id);
+    socket.broadcast.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    console.log("new answer from ", socket.id);
+    socket.broadcast.emit("answer", answer);
+  });
+
+  socket.on("icecandidate", (candidate) => {
+    console.log("new ice candidate from ", socket.id);
+    socket.broadcast.emit("icecandidate", candidate);
+  });
+});
+
+const puerto = process.env.PORT || 8080;
 server.listen(puerto, () => {
   console.log(`Conectado en el puerto http://localhost:${puerto}`);
 });
