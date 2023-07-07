421d90536df4fefe96d88c93b628e4bf09e5b58d
¨luis <luis.gonzalez.dpht@gmail.com>
2023-07-06 23:57:07 -0400

    ok

--- src/public/scripts/compartirPantalla.js
+++ src/public/scripts/compartirPantalla.js
@@ -1,42 +1,48 @@
-let videoElement = document.getElementById('videoElement');
-    let displayMediaStream = null;
-  
-    function startScreenSharing() {
-      if (navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
-        navigator.mediaDevices.getDisplayMedia({ video: true })
-          .then(function(stream) {
-            displayMediaStream = stream;
-            videoElement.srcObject = stream;
-          })
-          .catch(function(error) {
-            console.error('Error al compartir la pantalla: ', error);
-          });
-      } else {
-        console.error('La API de captura de medios no está soportada en este navegador.');
-      }
-    }
-    
-    function startCamera() {
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-          .then(function(stream) {
-            displayMediaStream = stream;
-            videoElement.srcObject = stream;
-          })
-          .catch(function(error) {
-            console.error('Error al activar la cámara: ', error);
-          });
-      } else {
-        console.error('La API de captura de medios no está soportada en este navegador.');
-      }
-    }
-    
-    function stopMedia() {
-      if (displayMediaStream) {
-        displayMediaStream.getTracks().forEach(function(track) {
-          track.stop();
-        });
-        videoElement.srcObject = null;
-        displayMediaStream = null;
-      }
-    }
+let videoElement = document.getElementById("videoElement");
+let displayMediaStream = null;
+
+function startScreenSharing() {
+  if (navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
+    navigator.mediaDevices
+      .getDisplayMedia({ video: true })
+      .then(function (stream) {
+        displayMediaStream = stream;
+        videoElement.srcObject = stream;
+      })
+      .catch(function (error) {
+        console.error("Error al compartir la pantalla: ", error);
+      });
+  } else {
+    console.error(
+      "La API de captura de medios no está soportada en este navegador."
+    );
+  }
+}
+
+function startCamera() {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(function (stream) {
+        displayMediaStream = stream;
+        videoElement.srcObject = stream;
+      })
+      .catch(function (error) {
+        console.error("Error al activar la cámara: ", error);
+      });
+  } else {
+    console.error(
+      "La API de captura de medios no está soportada en este navegador."
+    );
+  }
+}
+
+function stopMedia() {
+  if (displayMediaStream) {
+    displayMediaStream.getTracks().forEach(function (track) {
+      track.stop();
+    });
+    videoElement.srcObject = null;
+    displayMediaStream = null;
+  }
+}
