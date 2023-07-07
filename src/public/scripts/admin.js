8f235c67c218ed98f23aed559eed3734eca8ffd9
¨luis <luis.gonzalez.dpht@gmail.com>
2023-07-07 00:54:00 -0400

    ok

--- src/public/scripts/admin.js
+++ src/public/scripts/admin.js
@@ -1,7 +1,7 @@
 // public/admin.js
 "use strict";
 const socket = io();
-const peer = new RTCPeerConnection();
+const peer = createPeer();
 
 const video = document.getElementById("client-screen");
 peer.addEventListener("track", (track) => {
@@ -21,6 +21,23 @@ peer.addEventListener("icecandidate", (event) => {
     socket.emit("icecandidate", event.candidate);
   }
 });
+
 socket.on("icecandidate", async (candidate) => {
   await peer.addIceCandidate(new RTCIceCandidate(candidate));
 });
+
+function createPeer() {
+  const peer = new RTCPeerConnection();
+
+  peer.addEventListener("track", (track) => {
+    video.srcObject = track.streams[0];
+  });
+
+  peer.addEventListener("icecandidate", (event) => {
+    if (event.candidate) {
+      socket.emit("icecandidate", event.candidate);
+    }
+  });
+
+  return peer;
+}
